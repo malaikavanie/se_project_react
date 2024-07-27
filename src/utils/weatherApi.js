8ccd08d2330de392d307f5887c16d78ca0579489
MALAIKA VANIE <MVANIE@MYYAHOO.COM>
2024-07-27 19:19:45 -0400

    drafting

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,18 +1,22 @@
-export const getWeather = ({ latitude, longitude }, apiKey) => {
-  return fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`
-  ).then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject(`Error: ${res.status}`);
-    }
-  });
+import { checkResponse } from "./utils";
+import { getCurrentPosition } from "./utils";
+
+export const getWeather = (apiKey) => {
+  return getCurrentPosition() // Call getCurrentPosition to get coordinates
+    .then(({ latitude, longitude }) => {
+      return fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`
+      );
+    })
+    .then((res) => checkResponse(res));
 };
 
 export const filterWeatherData = (data) => {
   const result = {};
-  result.temp = { F: data.main.temp };
+  result.temp = {
+    F: Math.round(data.main.temp),
+    C: Math.round(((data.main.temp - 32) * 5) / 9),
+  };
   result.city = data.name;
   result.type = getWeatherType(result.temp.F);
   result.condition = weatherCondition(data.weather[0].id);
@@ -38,17 +42,18 @@ const weatherCondition = (conditionCode) => {
   switch (true) {
     case Math.floor(conditionCode / 100) == 2:
       return "stormy";
+    case Math.floor(conditionCode / 100) == 3 ||
+      Math.floor(conditionCode / 100) == 5:
+      return "rainy";
+    case Math.floor(conditionCode / 100) == 6:
+      return "snowy";
+    case conditionCode === 741:
+      return "foggy";
     case conditionCode === 800:
       return "clear";
     case Math.floor(conditionCode / 100) == 8:
       return "cloudy";
-    case Math.floor(conditionCode / 100) == 5:
-      return "rainy";
-    case Math.floor(conditionCode / 100) == 3:
-      return "rainy";
-    case Math.floor(conditionCode / 100) == 6:
-      return "snowy";
-    case Math.floor(conditionCode / 100) == 7:
-      return "foggy";
+    default:
+      return "default";
   }
 };
